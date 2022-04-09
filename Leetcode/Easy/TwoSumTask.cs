011f15b76fcebf3287a83f3763a4b3a0cca3e214
Olesia Pashko <pashkoolesia1234@gmail.com>
2022-04-09 18:02:16 +0300

    add first solved task from leetcode

---
+++ Leetcode/Easy/TwoSumTask.cs
@@ -0,0 +1,54 @@
+namespace Leetcode.Easy
+{
+    using System;
+    using System.Collections.Generic;
+
+    //EXAMPLE
+
+    //INPUT
+    //int[] nums = new int[]{3,2,4};
+    //int target = 6;
+
+    //OUTPUT
+    //[1,2]
+    public class TwoSumTask
+    {
+        //Space complexity 0, time complexity O(n^2)
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] {i, j};
+                    }
+                }
+            }
+
+            throw new Exception("There is no valid answer");
+        }
+
+        //Space complexity O(n), time complexity O(n)
+        public static int[] TwoSumWithDictionary(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var currentNumber = nums[i];
+                if (dictionary.ContainsKey(target - currentNumber))
+                {
+                    return new[] {dictionary[target - currentNumber], i};
+                }
+
+                if (!dictionary.ContainsKey(currentNumber))
+                {
+                    dictionary.Add(currentNumber, i);
+                }
+            }
+
+            throw new Exception("There is no valid answer");
+        }
+    }
+}
